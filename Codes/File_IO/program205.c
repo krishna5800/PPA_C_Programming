253f6a0d7ae5f1104f92068cc742a221b364380d
Krishna Hitnalikar <krishnahitnalikar4@gmail.com>
2025-11-22 22:40:27 +0530

    FILE HANDLING CODES PPA

---
+++ Codes/File_IO/program205.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<fcntl.h>   
+
+int main()
+{
+    int fd = 0, iRet = 0;
+    char FileName[20];
+    char Data[50];      // buffer
+
+    printf("Enter name of file : \n");
+    scanf("%s", FileName);
+
+    fd = open(FileName, O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open\n");
+        return -1;
+    }
+
+    iRet = read(fd, Data, 5);
+
+    printf("%d bytes gets successfully read\n", iRet);
+
+    printf("Data from file : %s\n", Data);
+
+    close(fd);
+
+    return 0;
+}
